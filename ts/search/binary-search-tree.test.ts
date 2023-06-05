7bffc3b47d3e3b18246a80925b20129655353297
yama-sitter <dadayamism@gmail.com>
2023-06-05 22:15:05 +0900

    add delete method to binary search tree

--- ts/search/binary-search-tree.test.ts
+++ ts/search/binary-search-tree.test.ts
@@ -5,35 +5,98 @@ describe("BinarySearchTree", () => {
 
 	beforeEach(() => {
 		tree = new BinarySearchTree();
-		tree.insert(13);
-		tree.insert(3);
-		tree.insert(34);
-		tree.insert(21);
-		tree.insert(2);
-		tree.insert(5);
-		tree.insert(55);
 	});
 
 	describe("insert()", () => {
-		test("inserted result is intended tree structure", () => {
-			expect(tree.rootNode?.value).toEqual(13);
-			expect(tree.rootNode?.left?.value).toEqual(3);
-			expect(tree.rootNode?.right?.value).toEqual(34);
-			expect(tree.rootNode?.right?.left?.value).toEqual(21);
-			expect(tree.rootNode?.left?.left?.value).toEqual(2);
-			expect(tree.rootNode?.left?.right?.value).toEqual(5);
-			expect(tree.rootNode?.right?.right?.value).toEqual(55);
-			expect(tree.toArrayInOrder()).toStrictEqual([2, 3, 5, 13, 21, 34, 55]);
+		beforeEach(() => {
+			tree.insert(13);
+			tree.insert(3);
+			tree.insert(34);
+			tree.insert(21);
+			tree.insert(2);
+			tree.insert(5);
+			tree.insert(55);
+			tree.insert(1);
+		});
+
+		test("succeeds in inserting values", () => {
+			expect(tree.rootNode?.value).toStrictEqual(13);
+			expect(tree.rootNode?.left?.value).toStrictEqual(3);
+			expect(tree.rootNode?.right?.value).toStrictEqual(34);
+			expect(tree.rootNode?.right?.left?.value).toStrictEqual(21);
+			expect(tree.rootNode?.left?.left?.value).toStrictEqual(2);
+			expect(tree.rootNode?.left?.right?.value).toStrictEqual(5);
+			expect(tree.rootNode?.right?.right?.value).toStrictEqual(55);
+			expect(tree.rootNode?.left?.left?.left?.value).toStrictEqual(1);
+			expect(tree.toArrayInOrder()).toStrictEqual([1, 2, 3, 5, 13, 21, 34, 55]);
 		});
 	});
 
 	describe("search()", () => {
-		test("succeed to search value", () => {
+		beforeEach(() => {
+			tree.insert(21);
+		});
+
+		test("succeeds in searching value", () => {
 			expect(tree.search(21)).toBeTruthy();
 		});
 
-		test.skip("failed to search value", () => {
-			expect(tree.search(89)).toBeFalsy();
+		test("failed to search value", () => {
+			expect(tree.search(55)).toBeFalsy();
+		});
+	});
+
+	describe("delete()", () => {
+		beforeEach(() => {
+			tree.insert(13);
+			tree.insert(3);
+			tree.insert(34);
+			tree.insert(21);
+			tree.insert(2);
+			tree.insert(5);
+			tree.insert(55);
+			tree.insert(1);
+		});
+
+		test("succeeds in deleting a node that has no child nodes ", () => {
+			expect(tree.rootNode?.right?.right?.value).toStrictEqual(55);
+			tree.delete(55);
+			expect(tree.rootNode?.right?.right).toStrictEqual(null);
+			expect(tree.rootNode?.right?.right?.value).toBeUndefined();
+		});
+
+		test("succeeds in deleting a node that has one child node ", () => {
+			expect(tree.rootNode?.left?.left?.value).toStrictEqual(2);
+			tree.delete(2);
+			expect(tree.rootNode?.left?.left?.value).toStrictEqual(1);
+		});
+
+		test("succeeds in deleting a node that has two child nodes ", () => {
+			expect(tree.rootNode?.right?.value).toStrictEqual(34);
+			expect(tree.rootNode?.right?.left?.value).toStrictEqual(21);
+			expect(tree.rootNode?.right?.right?.value).toStrictEqual(55);
+
+			tree.delete(34);
+
+			expect(tree.rootNode?.right?.value).toStrictEqual(55);
+			expect(tree.rootNode?.right?.left?.value).toStrictEqual(21);
+			expect(tree.rootNode?.right?.right).toStrictEqual(null);
+		});
+
+		test("succeeds in deleting a node that has two child nodes ", () => {
+			expect(tree.rootNode?.value).toStrictEqual(13);
+			expect(tree.rootNode?.left?.value).toStrictEqual(3);
+			expect(tree.rootNode?.right?.value).toStrictEqual(34);
+			expect(tree.rootNode?.right?.left?.value).toStrictEqual(21);
+			expect(tree.rootNode?.right?.right?.value).toStrictEqual(55);
+
+			tree.delete(13);
+
+			expect(tree.rootNode?.value).toStrictEqual(21);
+			expect(tree.rootNode?.left?.value).toStrictEqual(3);
+			expect(tree.rootNode?.right?.value).toStrictEqual(34);
+			expect(tree.rootNode?.right?.left).toStrictEqual(null);
+			expect(tree.rootNode?.right?.right?.value).toStrictEqual(55);
 		});
 	});
 });
--- ts/search/binary-search-tree.ts
+++ ts/search/binary-search-tree.ts
@@ -57,6 +57,47 @@ export class BinarySearchTree {
 		return this.#searchRecursive(value, node.right);
 	}
 
+	delete(value: number) {
+		this._rootNode = this.#deleteRecursive(value, this._rootNode);
+	}
+
+	#deleteRecursive(value: number, node: Node | null) {
+		if (node === null) return null;
+
+		if (value === node.value) {
+			// 削除対象ノードが子ノードを持たない or 右の子ノードだけを持つ場合
+			// 子ノードを持たない場合でも、node.rightがnullなので同様に処理できる
+			if (node.left === null) {
+				return node.right;
+			}
+			// 削除対象ノードが左の子ノードだけを持つ場合
+			else if (node.right === null) {
+				return node.left;
+			}
+
+			// 削除対象ノードが2つの子ノードを持つ場合
+			// 子ノードの最小値を探し、値を入れ替える
+			const min = this.#findMin(node.right);
+			node.value = min.value;
+			// 子ノードの最小値を削除する
+			node.right = this.#deleteRecursive(min.value, node.right);
+		}
+
+		// 探索
+		if (value < node.value) {
+			node.left = this.#deleteRecursive(value, node.left);
+		} else if (value > node.value) {
+			node.right = this.#deleteRecursive(value, node.right);
+		}
+
+		return node;
+	}
+
+	// ツリーの最小値 = 左の部分木の末端を探す
+	#findMin(node: Node): Node {
+		return node.left === null ? node : this.#findMin(node.left);
+	}
+
 	toArrayInOrder(node: Node | null = this.rootNode): number[] {
 		if (node === null) return [];
 
